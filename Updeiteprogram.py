f88be1368fa0cd69977086f010dedcb041156de9
NeodekvatDayz <82779973+NeodekvatDayz@users.noreply.github.com>
2023-08-03 15:27:05 +0300

    Update Updeiteprogram.py

--- Updeiteprogram.py
+++ Updeiteprogram.py
@@ -1,8 +1,11 @@
+import threading
+import requests
+import os
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
 # Подключаем окно
-Form, Window = uic.loadUiType("panm.ui")  #*!Изменить
+Form, Window = uic.loadUiType("update.ui")  #*!Изменить
 app = QApplication([])
 window = Window()
 form = Form()
@@ -18,6 +21,21 @@ def chek():
 
 #Обновление программы
 def download():
+    def down(nomer, url):
+        print(str(nomer) + 'поток ('+url+')\n')
+        (dirname, filename) = os.path.split(url)
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+           with open(filename, 'wb') as f:
+                r.raw.decode_content = True
+           print(str(nomer) + "поток закончил загрузку\n")
+
+    urls = ["https://codeload.github.com/NeodekvatDayz/TestUpdate/zip/refs/heads/main"]
+
+    for nomer, url in enumerate(urls):
+        threading.Thread(target=down, args=[nomer+1,url]).start()
+    print("Скачиваем файлы...")
+    #os.startfile('Open.bat')
     pass
 
 # Назначаем функции виджетов
